e28e9d4da503456419f99e764b0f56c9da02716f
Jonas <git@finkler.me>
2026-03-18 08:07:35 +0100

    Crop images before training, data augmentation, train last conv layer, use rocm gpu.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import config
 
 from src.detection.detection_state import DetectionState
-from src.audio.audio_listener import AudioListener
+# from src.audio.audio_listener import AudioListener
 from src.camera.camera_stream import CameraStream
 from src.detection.bird_detector import BirdDetector
 from src.detection.audio_species_identifier import SpeciesIdentifier
